f354af2c1ce4f0e2f20d75a28cf1f006c7a3a31b
Juan Antonio Cuesta Tejera <juancuesta1987@gmail.com>
2024-05-19 18:28:40 +0200

    codigo inicial

---
+++ src/edtarea6ra04/EDtarea6RA04.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package edtarea6ra04;
+
+/**
+ *
+ * @author juanc
+ */
+public class EDtarea6RA04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Descuentos miCarrito = new Descuentos();
+
+        miCarrito.aplicarDescuento(100, 5);
+    }
+
+}
+
+
